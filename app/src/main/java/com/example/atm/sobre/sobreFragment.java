8c7c6c19ca15f895fcf63f1962f5f515b5c9aba8
kaio <kaiogabryelsivad>
2022-07-30 18:06:44 -0300

    upload projeto

---
+++ app/src/main/java/com/example/atm/sobre/sobreFragment.java
@@ -0,0 +1,93 @@
+package com.example.atm.sobre;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.atm.R;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link sobreFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class sobreFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public sobreFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment sobreFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static sobreFragment newInstance(String param1, String param2) {
+        sobreFragment fragment = new sobreFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+String descricao = "Quero te ver de novo, poder te abraçar forte e fazer cara de quem tá nem aí contigo e fingir que nem lembrava de ti até te encontrar. Quero um encontro casual e trocar umas palavras impensadas, quem sabe relembrar aqueles momentos nossos, quem sabe te arrancar aquele sorriso que acho lindo. Quero saborear teu beijo, com desejo… Aquele mesmo que tive ao te ver me esperando na";
+        Element VERSAO = new Element();
+        VERSAO.setTitle("versao 1,0");
+
+       return new AboutPage(getActivity())
+
+       .setImage(R.drawable.logo)
+       .setDescription( descricao)
+       .addGroup("entre em comtato")
+       .addEmail("atendemento@atmcosutoria.com.br","em vie um emlil")
+       .addWebsite("https://github.com/medyo/android-about-page" + "asese noso sat")
+       .addGroup("redes sociais" )
+       .addFacebook("https://pt-br.facebook.com/FECBrasil/" + "fecbook")
+       .addInstagram("jamiltondamasceno" + "instragram")
+       .addTwitter("jamiltondamasceno" + "twitter")
+       .addGitHub("jamiltondamasceno" + "Github")
+       .addYoutube("jamiltondamasceno" + "youtube")
+       .addYoutube("com.directv.dtvlatam" + "baixe o noso aplicativo")
+
+       .addItem(VERSAO)
+       .create();
+
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_sobre, container, false);
+
+    }
+}
